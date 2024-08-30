a8313b0f7f99489db7b9f8af53e8e7f5cd4a8d22
jerolomb <jerome.lombardi@cases.lu>
2024-08-30 09:40:42 +0200

    update regional are

--- globals.py
+++ globals.py
@@ -2,16 +2,15 @@ from django.utils.translation import gettext_lazy as _
 
 # define the regional sectors of the country
 REGIONAL_AREA = [
-    ("LU_CA", _("Capellen")),
-    ("LU_CL", _("Clervaux")),
-    ("LU_DI", _("Diekirch")),
-    ("LU_EC", _("Echternach")),
-    ("LU_ES", _("Esch-sur-Alzette")),
-    ("LU_GR", _("Grevenmacher")),
-    ("LU_LU", _("Luxembourg")),
-    ("LU_ME", _("Mersch")),
-    ("LU_RE", _("Redange")),
-    ("LU_REM", _("Remich")),
-    ("LU_VI", _("Vianden")),
-    ("LU_WI", _("Wiltz")),
+    ("BE_AN", _("Province of Antwerp")),
+    ("BE_LI", _("Province of Limburg")),
+    ("BE_WF", _("Province of West Flanders")),
+    ("BE_EF", _("Province of East Flanders")),
+    ("BE_HA", _("Province of Hainaut")),
+    ("BE_LE", _("Province of Liège")),
+    ("BE_LU", _("Province of Luxembourg")),
+    ("BE_NA", _("Province of Namur")),
+    ("BE_FB", _("Province of Flemish Brabant")),
+    ("BE_WB", _("Province of Walloon Brabant")),
+    ("BE_BR", _("Brussels-Capital Region")),
 ]
