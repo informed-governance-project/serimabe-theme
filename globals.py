67a70df4ed669aee369ceb83c04f16957ea0f9b3
jerolomb <jerome.lombardi@cases.lu>
2024-08-30 08:54:51 +0200

    add region list in the theme

---
+++ globals.py
@@ -0,0 +1,17 @@
+from django.utils.translation import gettext_lazy as _
+
+# define the regional sectors of the country
+REGIONAL_AREA = [
+    ("LU_CA", _("Capellen")),
+    ("LU_CL", _("Clervaux")),
+    ("LU_DI", _("Diekirch")),
+    ("LU_EC", _("Echternach")),
+    ("LU_ES", _("Esch-sur-Alzette")),
+    ("LU_GR", _("Grevenmacher")),
+    ("LU_LU", _("Luxembourg")),
+    ("LU_ME", _("Mersch")),
+    ("LU_RE", _("Redange")),
+    ("LU_REM", _("Remich")),
+    ("LU_VI", _("Vianden")),
+    ("LU_WI", _("Wiltz")),
+]
