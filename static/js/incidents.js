58fc8a5ec19be1696c3093c264b12384e057d62d
Juan Rocha <juan.rocha@nc3.lu>
2024-09-19 11:19:57 +0200

    [template]Improved model  contact render

--- static/js/incidents.js
+++ static/js/incidents.js
@@ -97,4 +97,23 @@ $(document).ready(function () {
         let deleteUrl = deleteUrlBase.replace('0', incidentId);
         modalDeleteButton.attr('href', deleteUrl);
     });
+
+    $('.contacts_incident').on("click", function () {
+        let $this = $(this);
+        const contacts = $this.data('contacts');
+        if (contacts.contact_name == contacts.technical_name) {
+            $('#technical-card').remove();
+            let both_subtitle = $('#translated-both-contact-text').text();
+            $('#card-subtitle-contact').text(both_subtitle);
+        }
+
+        $('#contact-name').text(contacts.contact_name);
+        $('#contact-jobtitle').text(contacts.contact_jobtitle);
+        $('#contact-email').text(contacts.contact_email);
+        $('#contact-telephone').text(contacts.contact_telephone);
+        $('#technical-name').text(contacts.technical_name);
+        $('#technical-jobtitle').text(contacts.technical_jobtitle);
+        $('#technical-email').text(contacts.technical_email);
+        $('#technical-telephone').text(contacts.technical_telephone);
+    });
 });
