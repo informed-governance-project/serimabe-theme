1c023ca43245d7137f5e25699e062fbd48d26a03
Juan Rocha <juan.rocha@nc3.lu>
2024-11-05 12:07:30 +0100

    [security_objective][templates]Removed extra code

--- static/js/declaration.js
+++ static/js/declaration.js
@@ -1,23 +1,6 @@
-// populate the value in selctpickers
-
-const selectpickers = document.getElementsByClassName("selectpicker");
-
-for(selectpicker of selectpickers) {
-    var i;
-    values = [];
-    if (selectpicker.hasAttribute("id")){
-        id = selectpicker.getAttribute("id")
-        for (i = 0; i < selectpicker.length; i++) {
-            if (selectpicker[i].hasAttribute("checked")) {
-                if(selectpicker[i].value != ''){
-                    values.push(selectpicker[i].value)
-                }
-            }
-        }
-        $('#'+id).selectpicker('val', values);
-    }
-}
 $(document).ready(function () {
+    $('.selectpicker').selectpicker();
+
     $(".incident_detection_date").parent().on("dp.change", function (e) {
         if ( $('.incident_starting_date').parent().data("DateTimePicker")){
             $('.incident_starting_date').parent().data("DateTimePicker").clear();
