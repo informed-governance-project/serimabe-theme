c217eb9ac3c0acc6dd703d57196ebc48876fdb3a
Juan Rocha <juan.rocha@nc3.lu>
2024-12-04 13:14:33 +0100

    [reporting][templates]Added dashboard filter and datatables

--- static/js/reporting_dashboard.js
+++ static/js/reporting_dashboard.js
@@ -3,6 +3,34 @@ $(document).ready(function () {
     const generateButton = $("#generateButton");
     const checkboxesDisabled = checkboxes.filter(':disabled');
     const checkAllInput = $("#select_all_companies");
+    const companyTableForm = $("#companyTableForm");
+
+    let table = $('#reporting-table').DataTable({
+        dom: 'rt<"table_controls mt-3 bottom d-flex justify-content-between lh-1 small"lip><"clear">',
+        autoWidth: false,
+        paging: true,
+        searching: false,
+        order: [[1, 'asc']],
+        columnDefs: [
+            {
+                targets: 0,
+                orderable: false,
+            },
+            {
+                targets: 1,
+                orderable: true,
+                type: 'string-utf8'
+            },
+            {
+                targets: 2,
+                orderable: true,
+                type: 'string-utf8'
+            },
+        ]
+    });
+
+    displayPagination(table);
+
 
     function updateCheckAll() {
         checkAllInput.prop('checked', checkboxes.not(":disabled").length === checkboxes.not(":disabled").filter(":checked").length);
@@ -11,6 +39,11 @@ $(document).ready(function () {
     function processCheckboxSelection(checkbox) {
         generateButton.prop("disabled", !checkboxes.is(":checked"));
     }
+
+    function displayPagination(table) {
+        let rowCount = table.data().length;
+        if (rowCount <= 10) $('.table_controls').addClass("d-none");
+      }
 
     checkboxes.on("change", function () {
         updateCheckAll();
@@ -25,9 +58,17 @@ $(document).ready(function () {
 
     if (checkboxes.length > 0) {
         processCheckboxSelection(checkboxes.filter(':checked').first());
+    } else {
+        generateButton.prop("disabled", true);
     }
 
     if (checkboxesDisabled.length === checkboxes.length) {
         checkAllInput.prop('disabled', true);
     }
+
+    generateButton.on('click', function () {
+        if (companyTableForm.length) {
+            companyTableForm[0].submit();
+        }
+    });
 });
