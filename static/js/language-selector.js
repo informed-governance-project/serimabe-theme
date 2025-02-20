a9a3621f16e657202718e8966f33f500573669a9
Juan Rocha <juan.rocha@nc3.lu>
2025-02-20 08:53:07 +0100

    [template]Improved language selector

--- static/js/language-selector.js
+++ static/js/language-selector.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("language_selector").addEventListener("change", function(evt) {
         evt.preventDefault();
-        document.getElementById("language_selector").parentNode.submit();
+        document.getElementById("set_language_form").submit();
     });
 });
