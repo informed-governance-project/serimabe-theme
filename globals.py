780a33bf2a20a8ee6b338384835223cc488fe9d2
Juan Rocha <juan.rocha@nc3.lu>
2025-02-27 10:02:07 +0100

    [globals] Updated regional areas

--- globals.py
+++ globals.py
@@ -1,15 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+
 # define the regional sectors of the country
 REGIONAL_AREA = [
-    ("LU_CA", "Capellen"),
-    ("LU_CL", "Clervaux"),
-    ("LU_DI", "Diekirch"),
-    ("LU_EC", "Echternach"),
-    ("LU_ES", "Esch-sur-Alzette"),
-    ("LU_GR", "Grevenmacher"),
-    ("LU_LU", "Luxembourg"),
-    ("LU_ME", "Mersch"),
-    ("LU_RE", "Redange"),
-    ("LU_REM", "Remich"),
-    ("LU_VI", "Vianden"),
-    ("LU_WI", "Wiltz"),
+    ("BE_AN", _("Province of Antwerp")),
+    ("BE_LI", _("Province of Limburg")),
+    ("BE_WF", _("Province of West Flanders")),
+    ("BE_EF", _("Province of East Flanders")),
+    ("BE_HA", _("Province of Hainaut")),
+    ("BE_LE", _("Province of Liège")),
+    ("BE_LU", _("Province of Luxembourg")),
+    ("BE_NA", _("Province of Namur")),
+    ("BE_FB", _("Province of Flemish Brabant")),
+    ("BE_WB", _("Province of Walloon Brabant")),
+    ("BE_BR", _("Brussels-Capital Region")),
 ]
