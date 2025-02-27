aedc5e7d568c86826b14676be108d61ca99c17fd
Juan Rocha <juan.rocha@nc3.lu>
2025-02-27 11:47:46 +0100

    [js] Added cookie retrieval and loading spinner functions

--- static/js/base.js
+++ static/js/base.js
@@ -28,3 +28,27 @@ $(document).ready(function () {
     });
   });
 })
+
+window.getCookie = function(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
+window.load_spinner = function(){
+  $('#loading-spinner').removeClass('d-none').fadeIn();
+}
+
+window.stop_spinner = function() {
+  $('#loading-spinner').fadeOut().addClass('d-none');
+}
