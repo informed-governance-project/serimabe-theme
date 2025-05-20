741f6ef3496b798a47844a5a393292db8a575299
Juan Rocha <juan.rocha@nc3.lu>
2025-05-20 16:47:55 +0200

    [Accessibility] Improved code formatting for better readability and consistency, fix informed-governance-project/NISINP#390

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.is-valid').forEach(function (element) {
-    element.classList.remove('is-valid');
+  element.classList.remove('is-valid');
 });
 
 // Catch session expiration and redirect to login page 
@@ -18,37 +18,41 @@ $(document).ajaxError(function (event, xhr) {
 var datatableTranslations = {
   info: gettext("Showing _START_ to _END_ of _TOTAL_ entries"),
   lengthMenu: gettext("_MENU_ entries per page"),
-  aria: {orderable: gettext(": Activate to sort")},
+  aria: { orderable: gettext(": Activate to sort") },
 };
 
 
 // initialize all tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl, {delay:200})
+  return new bootstrap.Tooltip(tooltipTriggerEl, { delay: 200 })
 })
 
 $(document).ready(function () {
-  $('.create_so_declaration').on( "click", function() {
+  $('.create_so_declaration').on("click", function () {
     var $popup = $("#create_so_declaration");
     var popup_url = '/securityobjectives/create';
 
     $(".modal-dialog", $popup).load(popup_url, function () {
-        $popup.modal("show");
+      $popup.modal("show");
     });
   });
 
-  $('.import_so_declaration').on( "click", function() {
+  $('.import_so_declaration').on("click", function () {
     var $popup = $("#import_so_declaration");
     var popup_url = '/securityobjectives/import';
 
     $(".modal-dialog", $popup).load(popup_url, function () {
-        $popup.modal("show");
+      $popup.modal("show");
     });
   });
 
   // Accesibility hack to be compliant with RGAA 11.2
   $('[data-bs-toggle="tooltip"]').filter('input, textarea, i').removeAttr('aria-label');
+
+  $('input[placeholder]').each(function () {
+    $(this).attr('title', $(this).attr('placeholder'));
+  });
 
   // Accesibility hack to be compliant with RGAA 10.8
   $('label.visually-hidden').remove();
