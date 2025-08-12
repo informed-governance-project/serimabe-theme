0bdb8e5ecdebda85b36d8ceaad3b8eff1399f93c
Juan Rocha <juan.rocha@nc3.lu>
2025-08-12 10:24:43 +0200

    [NI][declaration]Changed the form buttons behavior, fix informed-governance-project/NISINP#395

--- static/js/declaration.js
+++ static/js/declaration.js
@@ -2,24 +2,39 @@ $(document).ready(function () {
     $('.selectpicker').selectpicker();
 
     $(".incident_detection_date").parent().on("dp.change", function (e) {
-        if ( $('.incident_starting_date').parent().data("DateTimePicker")){
+        if ($('.incident_starting_date').parent().data("DateTimePicker")) {
             $('.incident_starting_date').parent().data("DateTimePicker").clear();
             $('.incident_starting_date').parent().data("DateTimePicker").maxDate(e.date);
+        }
+    });
+
+    $('#wizard-next-btn').on('click', function (event) {
+        const form = $(this).closest('form')[0];
+        const lastStep = $(this).data('last-step');
+        const currentStep = $(this).data('current-step');
+
+        if (!form.checkValidity()) {
+            form.reportValidity();
+            return;
+        }
+
+        if (lastStep === currentStep) {
+            load_spinner();
         }
     });
 
     let allTextarea = $('textarea');
 
     if (allTextarea.length > 0) {
-        allTextarea.each(function() {
+        allTextarea.each(function () {
             if ($(this).prop('disabled')) $(this).attr('rows', '10');
         });
 
-        allTextarea.on('focus', function() {
+        allTextarea.on('focus', function () {
             if (!$(this).prop('disabled')) $(this).attr('rows', '10');
         });
 
-        allTextarea.on('blur', function() {
+        allTextarea.on('blur', function () {
             if (!$(this).prop('disabled')) $(this).attr('rows', '3');
         });
     }
