a49f76c14fd3e8d68a4a2ec1d34e8eb17329245e
Juan Rocha <juan.rocha@nc3.lu>
2025-10-20 09:52:51 +0200

    [JS]moved getCookie function to base.js for reuse

--- static/js/base.js
+++ static/js/base.js
@@ -88,6 +88,22 @@ $(document).ready(function () {
   $('label.visually-hidden').remove();
 })
 
+window.getCookie = function (name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 window.load_spinner = function(){
   $('#loading-spinner').removeClass('d-none').fadeIn();
 }
