b32f8b37630c1e0622e5500810ec4bc13bc9905b
Juan Rocha <juan.rocha@nc3.lu>
2026-01-28 13:10:00 +0100

    [cookiebanner] Improved version handling and streamline cookie data processing.  comment informed-governance-project/NISINP#617

--- static/js/cookie_banner.js
+++ static/js/cookie_banner.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   let keyValue = document.cookie.match('(^|;) ?cookiebanner=([^;]*)(;|$)');
   let cookiebannerCookie = keyValue ? decodeURIComponent(keyValue[2]) : null;
+  const cookiebanner_version = JSON.parse($('#cookiebanner_version').text());
 
-  if (cookiebannerCookie && !window.cookiebanner_version) return;
-  if (cookiebannerCookie && window.cookiebanner_version){
-    if (window.cookiebanner_version == 0) return;
-    if (typeof cookiebannerCookie =="string" && window.cookiebanner_version == 0) return
+  if (cookiebannerCookie && !cookiebanner_version) return;
+  if (cookiebannerCookie && cookiebanner_version){
+    if (cookiebanner_version == 0) return;
+    if (typeof cookiebannerCookie =="string" &&cookiebanner_version == 0) return
     if (isJson(cookiebannerCookie)) {
       parsed_cookie = JSON.parse(cookiebannerCookie)
-      if (parsed_cookie.version && parsed_cookie.version == window.cookiebanner_version) return;
+      if (parsed_cookie.version && parsed_cookie.version == cookiebanner_version) return;
     }
   }
 
   $('#cookiebannerModal').modal("show");
 
   $(".cookiebannerSubmit").click(function (e) {
+    const cookiegroups = JSON.parse(
+      $("#cookiegroups-data").text()
+    );
+
     let enable_cookies;
     if (this.name === 'enable_all') {
       enable_cookies = cookiegroups.map((x) => x.id);
@@ -31,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let cookieData = {
       accepted: true,
       groups: enable_cookies,
-      version: window.cookiebanner_version ? window.cookiebanner_version : 0
+      version: cookiebanner_version ? cookiebanner_version : 0
     };
 
     let value = encodeURIComponent(JSON.stringify(cookieData));
