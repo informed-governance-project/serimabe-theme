88d1681d278555062cc7253b024a89ecba5d327a
Juan Rocha <juan.rocha@nc3.lu>
2026-01-29 09:35:23 +0100

    [NI] Restored reviewStatusChange function

--- static/js/declaration.js
+++ static/js/declaration.js
@@ -169,3 +169,15 @@ $(document).ready(function () {
     }
   });
 });
+
+function reviewStatusChange(select) {
+  const $this = $(select);
+  const value = $this.val();
+
+  $this.removeClass("fw-bold bg-failed bg-passed text-white");
+  if (value === "PASS") {
+    $this.addClass("fw-bold bg-passed text-white");
+  } else if (value === "FAIL") {
+    $this.addClass("fw-bold bg-failed text-white");
+  }
+}
