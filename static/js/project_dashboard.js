3e755ced42b9c8112e6f5666d5395682cf934556
Juan Rocha <juan.rocha@nc3.lu>
2026-03-18 14:42:35 +0100

    [reporting][project_dashboard]Added update company_project selects, comment informed-governance-project/NISINP#453

--- static/js/project_dashboard.js
+++ static/js/project_dashboard.js
@@ -10,4 +10,37 @@ $(document).ready(function () {
     }
   );
 
+  $(document).on("change", ".company-project-input", function () {
+    onChangeCompanyProject(this);
+  });
+
 });
+
+function onChangeCompanyProject(input) {
+  const $this = $(input)
+  const id = $this.data("company-project-id");
+  const csrftoken = getCsrftoken();
+  const url = `/reporting/company_project/${id}/update`
+  const fieldName = $this.attr("name");
+  const fieldValue = $this.is(":checked");
+  const data = {
+    field: fieldName,
+    value: fieldValue,
+  }
+  $this.prop("disabled", true);
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    headers: {
+      "X-CSRFToken": csrftoken
+    },
+    error: function (error) {
+      console.log(error);
+    },
+    complete: function () {
+      $this.prop("disabled", false);
+    }
+  });
+}
