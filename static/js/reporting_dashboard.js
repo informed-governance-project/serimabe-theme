e126fab495cdb5d613d1f60ceb9b6e4f1e916ede
Juan Rocha <juan.rocha@nc3.lu>
2026-03-27 12:01:53 +0100

    [Reporting]Fixed stop multiple download buttons, comment  informed-governance-project/NISINP#654

--- static/js/reporting_dashboard.js
+++ static/js/reporting_dashboard.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 
 
 
-  let pollTimer = null;
+  const pollTimers = {};
 
   function startPolling(projectId) {
     const downloadButton = $(`#download_reports[data-project-id="${projectId}"]`)
@@ -86,13 +86,13 @@ $(document).ready(function () {
       })
     })
 
-    stopPolling();
-    pollTimer = setInterval(function () {
+    stopPolling(projectId);
+    pollTimers[projectId] = setInterval(function () {
       $.get(`project/${projectId}/report/status`, function (data) {
         updateUI(projectId, data)
         if (isTerminalState(data.status)) {
           downloadButton.off("mouseenter.running mouseleave.running click.running")
-          stopPolling();
+          stopPolling(projectId);
           onTaskFinished(projectId,data);
         }
       }).fail(function () {
@@ -101,10 +101,15 @@ $(document).ready(function () {
     }, 1500);
   }
 
-  function stopPolling() {
-    if (pollTimer) {
-      clearInterval(pollTimer);
-      pollTimer = null;
+  function stopPolling(projectId) {
+    if (projectId) {
+      clearInterval(pollTimers[projectId]);
+      delete pollTimers[projectId];
+    } else {
+      Object.keys(pollTimers).forEach(id => {
+        clearInterval(pollTimers[id]);
+        delete pollTimers[id];
+      });
     }
   }
 
